e88cfcbdc24b771ef56e639c0b271f18886c4854
ayohizato <ayoshizato.ca@gmail.com>
2022-01-15 17:09:36 -0800

    home implementation

---
+++ src/components/Home.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt="amazon prime"
+        />
+
+        <div className="home_row">
+          <Product
+            id="1234567"
+            title="The Lean Startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product
+            id="1234568"
+            title="The Lean Startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+        </div>
+        <div className="home_row">
+          <Product
+            id="1234569"
+            title="The Lean Startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product
+            id="1234560"
+            title="The Lean Startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product
+            id="1234561"
+            title="The Lean Startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+        </div>
+        <div className="home_row">
+          <Product
+            id="1234562"
+            title="The Lean Startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCKOzlJKI2p04tbLmPixEOQpmkt-1m7ntc",
+    authDomain: "clone-83abe.firebaseapp.com",
+    projectId: "clone-83abe",
+    storageBucket: "clone-83abe.appspot.com",
+    messagingSenderId: "937724926271",
+    appId: "1:937724926271:web:9d351e3c1d53785b0c5b8a",
+    measurementId: "G-4SL32G4536"
+  };
